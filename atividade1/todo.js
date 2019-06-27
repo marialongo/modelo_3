f13a931d0de342e6d89a99babf4557a3f5b2b108
marialongo <https://github.com/marialongo/modelo_2.git>
2019-06-27 13:24:08 -0300

    mudando

---
+++ atividade1/todo.js
@@ -0,0 +1,18 @@
+function checaIdade(idade) {
+
+    return new Promise(function(resolve,rejeita) {
+        setTimeout(function() {
+            return idade >= 18 ? resolve() : rejeita ();
+
+        }, 2000);
+    });
+}
+
+
+checaIdade(20)
+.then(function(){
+    console.log("maior que 18");
+})
+.catch(function(){
+    console.log("menor que 18");
+});
